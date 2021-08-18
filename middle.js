cf4b22ea55155b8ebafa2e2650cec3a97f28196d
Sam-Fay <sam.fay.1984@gmail.com>
2021-08-18 19:00:50 -0400

    middle

---
+++ middle.js
@@ -0,0 +1,21 @@
+const middle = function(array) {
+    evenArrayMiddleIndex1 = [];
+    let evenArrayMiddleIndex2 = [];
+    let newArray = [];
+    if (array.length % 2 === 0) {
+      evenArrayMiddleIndex1 = array.length / 2
+      evenArrayMiddleIndex2 = (array.length / 2) - 1;
+      newArray.push(array[evenArrayMiddleIndex1])
+      newArray.push(array[evenArrayMiddleIndex2])
+      
+      return 'The middle elements of this array are: ' + newArray;
+    }
+    else {
+      let oddArrayMiddleIndex = [];
+      oddArrayMiddleIndex = (array.length + 1) / 2;
+      newArray.push(array[oddArrayMiddleIndex])
+      return 'The middle element of this array is: ' + oddArrayMiddleIndex;
+    }
+  };
+console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14]))
+  
