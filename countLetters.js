03b36fcbec48e9793caf5665dfeca50d441b3766
Sam-Fay <sam.fay.1984@gmail.com>
2021-08-18 21:01:52 -0400

    CountLetters

--- countLetters.js
+++ countLetters.js
@@ -1,2 +1,26 @@
-countLetters("lighthouse in the house");
-console.log(Letters)
+const assertEqual = function(actual, expected) {
+    let isTrue = actual === expected;
+    if (isTrue) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else if (!isTrue) {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  };
+  
+  const countLetters = function(string) {
+    const counter = {};
+    for (let letter of string) {
+      if (counter[letter]) {
+        counter[letter] += 1;
+      } else {
+        counter[letter] = 1;
+      }
+    }
+    return counter;
+  };
+  
+  assertEqual(countLetters('LHL')['L'], 2);
+  assertEqual(countLetters('LHL')['H'], 1);
+  
+  console.log(countLetters('LHL')['L']);
+  
