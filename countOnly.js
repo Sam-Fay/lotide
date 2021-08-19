3a9a8a2634513bf41114d960d69c027d311e6947
Sam-Fay <sam.fay.1984@gmail.com>
2021-08-18 20:38:14 -0400

    Loop over allItems

--- countOnly.js
+++ countOnly.js
@@ -48,3 +48,13 @@ console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   assertEqual(result1["Karima"], undefined);
   assertEqual(result1["Fang"], 2);
   assertEqual(result1["Agouhanna"], undefined); 
+
+  const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    console.log(item);
+  }
+
+  return results;
+}
