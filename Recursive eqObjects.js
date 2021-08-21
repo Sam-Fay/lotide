ee473139d42fbe7c69df9f04e6d50c0168ec011a
Sam Fay <sam.fay.1984@gmail.com>
2021-08-21 19:06:34 +0000

    Recursive eqObjects

---
+++ Recursive eqObjects.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚨🚨🚨 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  } else {
+    for (const key of keys1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) return false;
+      } else if (typeof object1[key] === 'object' && typeof object2[key] === 'object') {
+        if (!eqObjects(object1[key], object2[key])) return false;
+      } else {
+        if (object1[key] !== object2[key]) return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+
