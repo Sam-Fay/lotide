68c48c51de744c90cec703f4350df689e8860ec2
Sam Fay <sam.fay.1984@gmail.com>
2021-08-24 04:30:03 +0000

    npmsemver

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const shortlived = require ('./shortlived');
 
 module.exports = {
   head:   head,
   tail:   tail,
-  middle: middle
+  middle: middle,
+  shortlived:   shortlived
 };
---
+++ shortlived.js
@@ -0,0 +1,4 @@
+const obj = { x: 4, y: 1 };
+const {x,y} = obj;
+console.log(x); // 4
+console.log(y); // 1
