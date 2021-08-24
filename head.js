16e988b579344b320f92182f70e0a391917b7c9f
Sam Fay <sam.fay.1984@gmail.com>
2021-08-24 03:01:47 +0000

    head - mocha

--- head.js
+++ head.js
@@ -5,7 +5,7 @@ const head = function(arr) {
   return arr[0];
 }
 
-// testing code
+
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), undefined);
--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,23 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("First test :)", () => {
+    it("returns '5' for ['5']", () => {
+        assert.strictEqual(head(['5']), '10'); 
+      });
+});
+
+describe("Second test :)", () => {
+    it("returns '10' for ['10']", () => {
+        assert.strictEqual(head(['10']), '10'); 
+      });
+});
+
+
+
+
+
+/*const assertEqual = require("../assertEqual");
 const head = require("../head");
 
 
@@ -7,4 +26,4 @@ assertEqual(head([5,6,7]), 9); // it should be false
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello1"); // it should  be false
 assertEqual(head([]), undefined); // it should be true
 assertEqual(head(["a"]), "b"); // is should be fales 
-
+*/
